99241232d6a9d0245249122b63ef22c7cf4b4227
jakobpalmborg <jakob.palmborg@gmail.com>
2023-03-24 09:44:47 +0100

    show colors when correct, missplaced och incorrect

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,18 @@ import Footer from './components/Footer';
 import InputForm from './components/InputForm';
 
 function App() {
-  const [guessListLetters, setGuessListLetters] = useState([]);
+  const [guessListLetters, setGuessListLetters] = useState([
+    // this format comes back from API
+    // { letter: 'H', result: 'incorrect' },
+    // { letter: 'A', result: 'missplaced' },
+    // { letter: 'L', result: 'incorrect' },
+    // { letter: 'L', result: 'correct' },
+    // { letter: 'Å', result: 'incorrect' },
+  ]);
+  const [guessListFeedback, setGuessListFeedback] = useState({
+    correct: false,
+    missplaced: false,
+  });
 
   function handleSubmit(event, formData) {
     event.preventDefault();
@@ -17,18 +28,34 @@ function App() {
       formData.letter4,
     ]);
   }
+
   return (
     <>
       <Header />
 
       <ul className="flex justify-center gap-1 w-80 m-auto flex-wrap">
         {guessListLetters.map((item, index) => (
-          <li
-            className="flex items-center justify-center font-bold font-mono uppercase text-4xl one-letter border-2 border-black w-14 h-14"
+          <div
             key={index}
+            // logic to handle the result from api
+            // className={
+            //   item.result === 'correct'
+            //     ? 'bg-green-700'
+            //     : item.result === 'incorrect'
+            //     ? 'bg-yellow-500'
+            //     : 'bg-gray-400'
+            // }
           >
-            {item}
-          </li>
+            <li
+              className="flex items-center justify-center font-bold font-mono uppercase text-4xl one-letter border-2 border-black w-14 h-14"
+              key={index}
+            >
+              {
+                // change to item.letter when you have real data
+                item
+              }
+            </li>
+          </div>
         ))}
       </ul>
 

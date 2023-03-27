579d74b5325ae5ee0463cb6ce863e7d31cc0e4ba
jakobpalmborg <jakob.palmborg@gmail.com>
2023-03-27 08:34:41 +0200

    add handlebars highscore page

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,28 @@
 import express from 'express';
-// import { engine } from 'express-handlebars';
+import { engine } from 'express-handlebars';
 
 const app = express();
-// app.engine('handlebars', engine());
-// app.set('views', './templates');
-// app.set('partials', '/templates/partials');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './backend/templates/');
+app.set('partials', '/templates/partials');
 
 // app.get('/', (req, res) => {
-//   res.send('index');
+//   res.send('index page');
 // });
 
 // app.get('/about', (req, res) => {
-//   res.send('about');
+//   res.send('about page');
 // });
 
-// app.get('/highscore', (req, res) => {
-//   res.send('highscore');
+app.get('/highscore', (req, res) => {
+  res.render('highscore');
+});
+
+// app.get('*', (req, res) => {
+//   res.send('404 page not found');
 // });
 
-app.use(express.static('../dist'));
+app.use(express.static('dist'));
 
 app.listen(5080);

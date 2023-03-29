65de2dce93da026264c0153280f4db628a0c3fb5
jakobpalmborg <jakob.palmborg@gmail.com>
2023-03-29 16:06:47 +0200

    send guess from frontend and get feedback + render result

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,38 +3,30 @@ import { useState, useEffect } from 'react';
 import InputForm from './components/InputForm';
 
 function App() {
+  const [guess, setGuess] = useState('');
+
+  const [guessListLetters, setGuessListLetters] = useState([]);
+
   useEffect(() => {
     async function getFeedback() {
-      const res = await fetch('./api/feedback');
+      const res = await fetch(`./api/feedback?guess=${guess}`);
       const payload = await res.json();
-      console.log(payload);
+      setGuessListLetters([...guessListLetters, ...payload]);
     }
     getFeedback();
-  }, []);
-
-  const [guessListLetters, setGuessListLetters] = useState([
-    // this format comes back from API
-    // { letter: 'H', result: 'incorrect' },
-    // { letter: 'A', result: 'missplaced' },
-    // { letter: 'L', result: 'incorrect' },
-    // { letter: 'L', result: 'correct' },
-    // { letter: 'Å', result: 'incorrect' },
-  ]);
-  const [guessListFeedback, setGuessListFeedback] = useState({
-    correct: false,
-    missplaced: false,
-  });
+  }, [guess]);
 
   function handleSubmit(event, formData) {
     event.preventDefault();
-    setGuessListLetters([
-      ...guessListLetters,
-      formData.letter0,
-      formData.letter1,
-      formData.letter2,
-      formData.letter3,
-      formData.letter4,
-    ]);
+    setGuess(
+      [
+        formData.letter0,
+        formData.letter1,
+        formData.letter2,
+        formData.letter3,
+        formData.letter4,
+      ].join('')
+    );
   }
 
   return (
@@ -44,22 +36,19 @@ function App() {
           <div
             key={index}
             // logic to handle the result from api
-            // className={
-            //   item.result === 'correct'
-            //     ? 'bg-green-700'
-            //     : item.result === 'incorrect'
-            //     ? 'bg-yellow-500'
-            //     : 'bg-gray-400'
-            // }
+            className={
+              item.result === 'correct'
+                ? 'bg-green-700'
+                : item.result === 'incorrect'
+                ? 'bg-yellow-500'
+                : 'bg-gray-400'
+            }
           >
             <li
               className="flex items-center justify-center font-bold font-mono uppercase text-4xl one-letter border-2 border-black w-14 h-14"
               key={index}
             >
-              {
-                // change to item.letter when you have real data
-                item
-              }
+              {item.letter}
             </li>
           </div>
         ))}

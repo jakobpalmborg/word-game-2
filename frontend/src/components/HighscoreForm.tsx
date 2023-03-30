e76ca4ac2f78e57126d0ffaf08707c0f231eb0fc
jakobpalmborg <jakob.palmborg@gmail.com>
2023-03-30 10:13:54 +0200

    add handlesubmit and started writing the useeffect for highscore

--- frontend/src/components/HighscoreForm.tsx
+++ frontend/src/components/HighscoreForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function HighscoreForm() {
   const [highscoreFormData, setHighscoreFormData] = useState({
@@ -9,6 +9,15 @@ export default function HighscoreForm() {
     noDuplicate: false,
   });
 
+  //Started writing on this useEffect for posting highscore to DB
+  // useEffect(() => {
+  //   async function postHighscore() {
+  //     const res = await fetch(`./api/highscore`);
+  //     const payload = await res.json();
+  //   }
+  //   postHighscore();
+  // }, []);
+
   function handleChange(event) {
     setHighscoreFormData((prevData) => ({
       ...prevData,
@@ -16,10 +25,14 @@ export default function HighscoreForm() {
     }));
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className=" text-center">
       <h2 className="font-bold text-lg p-2">Highscore</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onChange={handleChange}
           type="text"
